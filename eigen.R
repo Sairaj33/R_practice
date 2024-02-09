2bbd88a650789dbfd0a2da474efa6093bf8ae56c
Sairaj33 <113412605+Sairaj33@users.noreply.github.com>
2024-02-09 14:09:55 +0530

    Add files via upload

---
+++ eigen.R
@@ -0,0 +1,20 @@
+A <- matrix(data=c(3,2,1,1,4,1,1,2,3),nrow=3,ncol=3)
+print(A)
+
+# Calculate eigenvalues and eigenvectors and normalised eigenvectors
+eigen_result <- eigen(A)
+
+# Eigenvalues
+eigenvalues <- eigen_result$values
+print("Eigenvalues:")
+print(eigenvalues)
+
+# Eigenvectors
+eigenvectors <- eigen_result$vectors
+print("Eigenvectors:")
+print(eigenvectors)
+
+# Normalize eigenvectors
+normalized_eigenvectors <- eigenvectors / sqrt(rowSums(eigenvectors^2))
+print("Normalized Eigenvectors:")
+print(normalized_eigenvectors)
